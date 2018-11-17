7aa3ecce8aed0a95ee399c6e29bc139c76376522
Brian Vanegas <brianvanegas@viajesaeromar.com>
2018-11-17 00:26:57 -0500

    final

--- src/supermercado/ListaLigada.java
+++ src/supermercado/ListaLigada.java
@@ -8,15 +8,12 @@ public class ListaLigada
     private Nodo[] cabezas, ultimos;
     private double[] acumulados;
     private int numColas;
-
     public ListaLigada(int pNumColas)
     {
         numColas = pNumColas;
-
         cabezas = new Nodo[numColas];
         ultimos = new Nodo[numColas];
         acumulados = new double[numColas];
-
         for(int i=0; i<numColas; i++)
         {
             cabezas[i] = new Nodo("", 0.0);
@@ -24,17 +21,14 @@ public class ListaLigada
             acumulados[i] = 0.0;
         }
     }
-
     public boolean ColaVacia(int pI)
     {
         return (cabezas[pI].GetLiga() == null);
     }
-
     public double GetAcumulado(int pI)
     {
         return acumulados[pI];
     }
-
     public void Encolar(int pI, String pCodigo, double pValor)
     {
         Nodo p = new Nodo(pCodigo, pValor);
@@ -42,18 +36,17 @@ public class ListaLigada
         ultimos[pI] = p;
         acumulados[pI] += p.getValor();
     }
-
     public String Desencolar(int pI)
     {
         String codDesencolado;
         Nodo p = cabezas[pI].GetLiga();
+        cabezas[pI].setLiga(p.GetLiga());
         codDesencolado = p.getCodigo();
         if(p == ultimos[pI]){
             ultimos[pI] = cabezas[pI];
         }
         return codDesencolado;
     }
-
     public void MostrarCola(int pI) {
         Nodo p = cabezas[pI].GetLiga();
         while (p != null) {
--- src/supermercado/Supermercado.java
+++ src/supermercado/Supermercado.java
@@ -6,48 +6,31 @@ import javax.swing.JOptionPane;
  * @author Brian Vanegas
  */
 public class Supermercado {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
-        int n;
-        int colaIndicada;
+        int n, colaIndicada;
         char opc;
         String aCod;
         double aVC;
-
-        do
-        {
-            n = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero de colas a procesar", "Entrada"));
-        }
-        while(n < 0);
+        do{
+            n = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero de colas a procesar"));
+        }while(n < 0);
 
         ListaLigada objColas = new ListaLigada(n);
-        do
-        {
-            System.out.println("\n");
-            do
-            {
+        do{
+            do{
                 colaIndicada = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese numero de la cola (-1 para salir):"));
-            }
-            while(colaIndicada < -1 || colaIndicada > n-1);
-            if(colaIndicada != -1)
-            {
-                do
-                {
+            }while(colaIndicada < -1 || colaIndicada > n-1);
+            if(colaIndicada != -1){
+                do{
                     opc = Menu(colaIndicada);
-                    System.out.println("\n");
                     switch (opc) {
                         case '1':
-                            //Encolar
                             aCod = JOptionPane.showInputDialog(null, "Ingrese código:", "Entrada", JOptionPane.INFORMATION_MESSAGE);
                             aVC = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese valor compra", "Entrada", JOptionPane.INFORMATION_MESSAGE));
                             objColas.Encolar(colaIndicada, aCod, aVC);
                             break;
                         case '2':
-                            //Desencolar
                             if(!objColas.ColaVacia(colaIndicada)) {
                                 JOptionPane.showInputDialog(null, "El codigo desencolado es "+objColas.Desencolar(colaIndicada));
                             }
@@ -60,28 +43,19 @@ public class Supermercado {
                             }
                             break;
                         case '4':
-                            //Mostrar Cola
                             System.out.println("El acumulado de la cola #"+ colaIndicada +" es $"+objColas.GetAcumulado(colaIndicada));
-                            break;
-                        case '5':
-                            //salir
-                            break;
-                        default:
                             break;
                     }
                 }while(opc != '5');
             }
-        }
-        while(colaIndicada != -1);
+        }while(colaIndicada != -1);
     }
-
     static char Menu(int pI)
     {
         char opcion;
         do{
-            opcion = JOptionPane.showInputDialog(null, "1. Encolar\n2. Desencolar\n3. Mostrar Cola\n4. Mostrar Acumulado\n5. Salir").charAt(0);
-        }
-        while(opcion < '1' || opcion > '5');
+            opcion = JOptionPane.showInputDialog(null, "Cola #"+pI+"\n1. Encolar\n2. Desencolar\n3. Mostrar Cola\n4. Mostrar Acumulado\n5. Salir").charAt(0);
+        }while(opcion < '1' || opcion > '5');
         return opcion;
     }
 }
